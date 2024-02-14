a911544118ab9665a252aa303e29014e600463d6
agisbertb <agisbert@iesebre.com>
2024-02-14 18:40:17 +0100

    Tipografia, markdown i TailwindCSS prose

--- resources/markdown/terms.md
+++ resources/markdown/terms.md
@@ -1,3 +1,12 @@
 # Terms of Service
 
-Edit this file to define the terms of service for your application.
+Més info a: https://github.com/acacha/wiki/blob/main/casteaching.md
+
+# TODO
+
+Primer a testejar?
+
+Veure Videos
+- Els usuaris poden veure vídeos -> users_can_view_videos
+- Els usuaris poden veure vídeos publicats -> users_can_view_published_videos
+- Els usuaris NO poden veure vídeos NO PUBLICATS -> users_cannot_view_unpublished_videos
--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,16 +1,20 @@
 <x-casteaching-layout>
-    <div class="flex flex-col">
-        <iframe
-            class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5"
-            src="https://www.youtube.com/embed/d3ecax4PcUE?si=d75CXa1VCXkC1eaC"
-            title="YouTube video player" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-20">
-            <h2>{{ $video->title }}</h2>
+    <div class="flex flex-col space-x-4 space-y-4 lg:space-x-4 lg:space-y-4 xl:space-x10 xl:space-y-5
+    2xl:space-x20 2xl:space-y-10 items-center">
+        <div class="min-w-full">
+            <iframe
+                class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5 w-full"
+                style="height: 75vh; width: 100%;"
+                src="https://www.youtube.com/embed/B8gyW3N3eGY?si=eYWPI-keCCa5lbAv"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen></iframe>
+            <h2 class="text-gray-900 uppercase font-bold text-2xl tracking-tight">
+                {{ $video->title }}
+            </h2>
         </div>
-        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-20">
-            <h3>{{ $video->description }}</h3>
+        <div class="prose-sm md:prose lg:prose-xl 2xl:prose-2xl">
+            {!! Str::markdown($video->description) !!}
         </div>
     </div>
 </x-casteaching-layout>
