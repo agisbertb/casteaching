4019b4c5e1927315c5e16ddb7e32aeee3e3ea364
agisbertb <agisbert@iesebre.com>
2024-02-14 17:15:54 +0100

    Aplicar Estils des de zero Criteris de disseny TailwindCSS i Laravel mix

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,7 +1,16 @@
 <x-casteaching-layout>
-    <p>{{ $video->title }}</p>
-    <ul>
-        <li>Description: {{ $video->description }}</li>
-        <li>Data: {{ $video->published_at->format('F j') }}</li>
-    </ul>
+    <div class="flex flex-col">
+        <iframe
+            class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5"
+            src="https://www.youtube.com/embed/d3ecax4PcUE?si=d75CXa1VCXkC1eaC"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-20">
+            <h2>{{ $video->title }}</h2>
+        </div>
+        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-20">
+            <h3>{{ $video->description }}</h3>
+        </div>
+    </div>
 </x-casteaching-layout>
