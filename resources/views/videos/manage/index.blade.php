6da47916f61f3e45278296907ed48d7f5d32f243
agisbertb <agisbert@iesebre.com>
2024-03-13 17:49:19 +0100

    Disseny del formulari de creació de vídeos

--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -2,19 +2,53 @@
     <div class="px-4 sm:px-6 lg:px-8">
 
         @can('videos_manage_create')
-        <form data-qa="form_video_create" action="" method="post">
+        <div class="p-4 mt-10">
+            <form data-qa="form_video_create">
+                <div class="space-y-12">
+                    <div class="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+                        <div>
+                            <h2 class="text-base font-semibold leading-7 text-gray-900">Vídeos</h2>
+                            <p class="mt-1 text-sm leading-6 text-gray-600">Informació bàsica del vídeo</p>
+                        </div>
 
-            <label for="title">Title</label>
-            <input id="title" name="title" type="text">
+                        <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
 
-            <label for="description">Description</label>
-            <textarea id="description" name="description"></textarea>
+                            <div class="col-span-full">
+                                <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                                <div class="mt-2">
+                                    <input id="title" name="title" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6" placeholder="Títol del vídeo"></input>
+                                </div>
+                                <p class="mt-3 text-sm leading-6 text-gray-600">Títol curt del nostre vídeo</p>
+                            </div>
 
-            <label for="url">URL</label>
-            <input id="url" name="url" type="text">
+                            <div class="col-span-full">
+                                <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
+                                <div class="mt-2">
+                                    <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6" placeholder="Description"></textarea>
+                                </div>
+                                <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+                            </div>
 
-            <button>Crear</button>
-        </form>
+                            <div class="sm:col-span-4">
+                                <label for="url" class="block text-sm font-medium leading-6 text-gray-900">URL</label>
+                                <div class="mt-2">
+                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-red-600 sm:max-w-md">
+                                        <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">http://</span>
+                                        <input type="text" name="website" id="url" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="www.youtube.com">
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="mt-6 flex items-center justify-end gap-x-6">
+                    <button type="submit" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Crear</button>
+                </div>
+            </form>
+        </div>
         @endcan
 
         <div class="mt-10 flow-root">
@@ -44,9 +78,9 @@
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $video->description }}</td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $video->url }}</td>
                             <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                                <a href="/videos/{{ $video->id }}" target="_blank" class="text-indigo-600 hover:text-indigo-900">Show</a>
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Delete</a>
+                                <a href="/videos/{{ $video->id }}" target="_blank" class="text-red-600 hover:text-red-900">Show</a>
+                                <a href="#" class="text-red-600 hover:text-red-900">Edit</a>
+                                <a href="#" class="text-red-600 hover:text-red-900">Delete</a>
                             </td>
                         </tr>
                         @endforeach
