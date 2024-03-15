63f7b2f4d63b37830de0f6f4c7fd3da2740f42d0
agisbertb <agisbert@iesebre.com>
2024-03-15 12:24:07 +0100

    Petit canvi al navbar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,15 +15,11 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link href="/videos/1">
-                        {{ __('Videos 1') }}
-                    </x-nav-link>
                     @can('videos_manage_index')
                         <x-nav-link href="/manage/videos" :active="request()->routeIs('manage.videos')">
                             {{ __('Manage Videos') }}
                         </x-nav-link>
                     @endcan
-
                     @can('users_manage_index')
                         <x-nav-link href="/manage/users" :active="request()->routeIs('manage.users')">
                             {{ __('Manage Users') }}
@@ -174,9 +170,6 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link href="/videos/1">
-                {{ __('Videos 1') }}
             </x-responsive-nav-link>
             @can('videos_manage_index')
             <x-responsive-nav-link href="/manage/videos" :active="request()->routeIs('manage.videos')">
