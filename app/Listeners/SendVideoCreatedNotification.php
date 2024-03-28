94d2a5a3ac313ae07b062c53058788c6eae0a79f
agisbertb <agisbert@iesebre.com>
2024-03-28 17:02:44 +0100

    129 Cuas Laravel

--- app/Listeners/SendVideoCreatedNotification.php
+++ app/Listeners/SendVideoCreatedNotification.php
@@ -2,12 +2,10 @@
 
 namespace App\Listeners;
 
-use App\Events\VideoCreated;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
 
-class SendVideoCreatedNotification
+class SendVideoCreatedNotification implements ShouldQueue
 {
     /**
      * Create the event listener.
