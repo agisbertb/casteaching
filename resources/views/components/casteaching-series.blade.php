49dc31280e2d124d1c64b5af2f17e1700a27e5fa
Andreu Gisbert Bel <91249151+agisbertb@users.noreply.github.com>
2024-05-31 19:35:49 +0200

    Afegit href a les series del welcome

--- resources/views/components/casteaching-series.blade.php
+++ resources/views/components/casteaching-series.blade.php
@@ -10,17 +10,19 @@
                 @foreach($series as $serie)
                     <article class="flex flex-col items-start justify-between p-4 transition-transform transform hover:scale-105 hover:shadow-lg hover:shadow-red-500/50 hover:rounded-2xl">
                         <div class="relative w-full">
-                            <img src="{{ asset('storage/' . $serie->image) }}" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
-                            <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+                            <a href="{{ route('series.show', $serie->id) }}">
+                                <img src="{{ asset('storage/' . $serie->image) }}" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
+                                <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+                            </a>
                         </div>
                         <div class="max-w-xl">
                             <div class="mt-8 flex items-center gap-x-4 text-xs">
                                 <time datetime="2020-03-16" class="text-gray-500">{{ $serie->formatted_for_humans_created_at }}</time>
-                                <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-red-600 hover:bg-gray-100">Screencasts</a>
+                                <a href="{{ route('series.show', $serie->id) }}" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-red-600 hover:bg-gray-100">Screencasts</a>
                             </div>
                             <div class="group relative">
                                 <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                                    <a href="#">
+                                    <a href="{{ route('series.show', $serie->id) }}">
                                         <span class="absolute inset-0"></span>
                                         {{ $serie->title }}
                                     </a>
@@ -33,7 +35,7 @@
                                 @endif
                                 <div class="text-sm leading-6">
                                     <p class="font-semibold text-gray-900">
-                                        <a href="#">
+                                        <a href="{{ route('series.show', $serie->id) }}">
                                             <span class="absolute inset-0"></span>
                                             {{ $serie->teacher_name }}
                                         </a>
